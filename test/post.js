2fb8f80bcaaeda1277330928a358a24d3117fcfa
yash <yashsharma205@gmail.com>
2016-07-20 22:42:50 +0530

    Completed token based authenticate

---
+++ test/post.js
@@ -0,0 +1,35 @@
+process.env.NODE_ENV = 'test';
+
+var mongoose 	= require("mongoose"),
+	Post 		= require('../app/models/Post'),
+	chai 		= require('chai'),
+	chaiHttp 	= require('chai-http'),
+	server 		= require('../server'),
+	should 		= chai.should();
+
+
+chai.use(chaiHttp);
+
+/*
+//Before each test we empty the database
+describe('Posts', function () {
+	beforeEach(function(done){
+		Post.remove({}, function(err){
+			done();
+		});
+	});
+});*/
+
+
+describe('/GET posts', function(){
+	it('it should GET all the posts', function(done){
+		chai.request(server)
+			.get('/posts')
+			.end(function(err, res){
+				res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('data');
+                done();
+			});
+	});
+});
