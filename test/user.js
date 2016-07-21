7309f3b38bd8fb47295c957c996eb95400ada4b3
Yash Sharma <yash_sharma@seologistics.com>
2016-07-21 17:23:13 +0530

    Add user test file

---
+++ test/user.js
@@ -0,0 +1,110 @@
+process.env.NODE_ENV = 'test';
+
+var mongoose 	= require("mongoose"),
+	User 		= require('../app/models/User'),
+	chai 		= require('chai'),
+	chaiHttp 	= require('chai-http'),
+	server 		= require('../server'),
+	should 		= chai.should();
+
+chai.use(chaiHttp);
+
+/* Test Register a new user */
+describe('/POST register', function(){
+
+	before(function(done){
+		User.remove({}, function(err){
+			done();
+		});
+	});
+
+	it('it should not Register a user without username and password', function(done){
+		var user = {
+			username: null,
+			password: null
+		}
+		chai.request(server)
+			.post('/users/signup')
+			.send(user)
+			.end(function(err, res){
+				res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('errors');
+                res.body.errors.should.have.property('username');
+                res.body.errors.username.should.have.property('type').eql('required');
+                res.body.errors.should.have.property('password');
+                res.body.errors.password.should.have.property('type').eql('required');
+                done();
+			});
+	});
+	it('it should Register a user', function(done){
+		var user = {
+			username: 'Testing Bot',
+			password: '123456'
+		}
+		chai.request(server)
+			.post('/users/signup')
+			.send(user)
+			.end(function(err, res){
+				res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql('User registered successfully');
+                res.body.user.should.have.property('username');
+                res.body.user.should.have.property('password');
+                done();
+			});
+	});
+	it('it should not Register a user with same username', function(done){
+		var user = {
+			username: 'Testing Bot',
+			password: '123456'
+		}
+		chai.request(server)
+			.post('/users/signup')
+			.send(user)
+			.end(function(err, res){
+				res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('code').eql(11000);
+                res.body.should.have.property('ok').eql(1);
+                done();
+			});
+	});
+});
+
+describe('/POST login', function(){
+	it('it should not login a user with wrong username and password', function(done){
+		var user = {
+			username: 'gibbrash',
+			password: 'gibbrash'
+		}
+		chai.request(server)
+			.post('/users/login')
+			.send(user)
+			.end(function(err, res){
+				res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('type');
+                res.body.should.have.property('message').eql('Authentication failed. User not found.');
+                done();
+			});
+	});
+
+	it('it should login a user', function(done){
+		var user = {
+			username: 'Testing Bot',
+			password: '123456'
+		}
+		chai.request(server)
+			.post('/users/login')
+			.send(user)
+			.end(function(err, res){
+				res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('user');
+                res.body.should.have.property('token');
+                done();
+			});
+	});
+});
